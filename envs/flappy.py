1b66eb3b5728e84ccfb8a8fc0ebc454837841fe0
hoto17296 <mail@hoto.me>
2017-05-17 13:03:28 +0900

    add bullet

--- envs/flappy.py
+++ envs/flappy.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pyglet
 from pyglet.gl import *
 import gym
@@ -10,6 +11,7 @@ class Flappy(gym.Env):
     def __init__(self):
         self.window = pyglet.window.Window(width=480, height=640)
         self.actions = spaces.Discrete(2)
+        self.barrage = []
         self.circle_radius = 10
 
     def _step(self, action):
@@ -25,14 +27,36 @@ class Flappy(gym.Env):
 
         self.state = [pos, accel]
 
+        for bullet in self.barrage:
+            bullet.step()
+        self.barrage = list(filter(lambda b:b.step(), self.barrage))
+
+        # ランダムで弾を追加
+        if np.random.uniform() > 0.99:
+            x = self.window.width
+            y = int(np.random.uniform(0, self.window.height))
+            radius = 20 + np.random.randn() * 10
+            speed = 0.5 + np.abs(np.random.randn())
+            self.barrage.append(Bullet(pos=(x, y), radius=radius, speed=speed))
+
+        # 弾幕の当たり判定
+        hit = False
+        for bullet in self.barrage:
+            x, y = self.window.width/2, self.state[0]
+            bx, by = bullet.pos
+            diff = ((bx - x) ** 2 + (by - y) ** 2) ** 0.5
+            if diff < bullet.radius:
+                hit = True
+
         reward = 1.0
-        done = pos < self.circle_radius or pos > self.window.height
+        done = hit or pos < self.circle_radius or pos > self.window.height
         info = {}
 
         return self.state, reward, done, info
 
     def _reset(self):
         self.state = [self.window.height - self.circle_radius, 0]
+        self.barrage = []
         return self.state
 
     def _render(self, mode='human', close=False):
@@ -52,10 +76,37 @@ class Flappy(gym.Env):
         geom.add_attr(Transform(translation=(self.window.width/2, pos)))
         geom.render()
 
+        for bullet in self.barrage:
+            bullet.render()
+
         self.window.flip()
 
     def render_background(self):
         glClearColor(1,1,1,1) # White
+
+
+class Bullet:
+    def __init__(self, pos, radius=20, speed=1):
+        self.pos = pos
+        self.radius = radius
+        self.speed = speed
+        self.geom = None
+
+    def step(self):
+        x, y = self.pos
+        x -= self.speed
+        self.pos = (x, y)
+        return x > 0
+
+    def render(self):
+        if self.geom is None:
+            self.geom = make_circle(self.radius)
+            self.trans = Transform()
+            self.geom.add_attr(self.trans)
+        x, y = self.pos
+        self.trans.set_translation(x, y)
+        self.geom.render()
+
 
 if __name__ == '__main__':
     from pyglet.window import key as KEY
